53449c058f01423a9914e78e91fd30426416df2e
Egor Sysoykin <esisoykin@gmail.com>
2019-09-26 00:18:56 +0300

    some fixes

--- main.cs
+++ main.cs
@@ -3,33 +3,36 @@ using System;
 public class main {
 
 	public static void Main(String[] args) {
-		double a, b, c;
+		double a = 0, b = 0, c = 0;
 		Console.Title = "Сысойкин Егор ИУ5-34Б";
+		bool flag = true;
 		if(args.Length == 3) {
 			Console.WriteLine("Taking A, B, C from args");
+			flag = false;
 			if(!Double.TryParse(args[0], out a) || !Double.TryParse(args[1], out b) || !Double.TryParse(args[2], out c)) {
 				Console.ForegroundColor = ConsoleColor.Red;
 				Console.WriteLine("Error parsing args");
-				return;
+				Console.ResetColor();
+				flag = true;
 			}
 		}
-		else {
+		if(flag) {
 			Console.WriteLine("Invalid args. Put in A, B, C from keyboard");
-			Console.Write("A= ");
+			Console.Write("A = ");
                 	while(!Double.TryParse(Console.ReadLine(), out a)) {
 				Console.ForegroundColor = ConsoleColor.Red;
 				Console.WriteLine("Error parsing A");
 				Console.ResetColor();
 				Console.Write("A= ");
 			}
-                	Console.Write("B= ");
+                	Console.Write("B = ");
                 	while(!Double.TryParse(Console.ReadLine(), out b)) {
 				Console.ForegroundColor = ConsoleColor.Red;
                                 Console.WriteLine("Error parsing B");
                                 Console.ResetColor();
                                 Console.Write("B= ");
 			}
-                	Console.Write("C= ");
+                	Console.Write("C = ");
                 	while(!Double.TryParse(Console.ReadLine(), out c)) {
 				Console.ForegroundColor = ConsoleColor.Red;
                         	Console.WriteLine("Error parsing C");
@@ -41,7 +44,7 @@ public class main {
 		double y1, y2;
 		double x1, x2, x3, x4;
 		double D = b*b - 4*a*c;
-		bool flag = false;
+		flag = false;
 		Console.ForegroundColor = ConsoleColor.Green;
 		if(D < 0) {
 			Console.ForegroundColor = ConsoleColor.Red;
@@ -69,6 +72,11 @@ public class main {
 		else {
 			y1 = (-b + Math.Sqrt(D))/(2*a);
 			y2 = (-b - Math.Sqrt(D))/(2*a);
+			if(y1 < 0 && y2 < 0) {
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("no real roots");
+				return;
+			}
 			if (y1 >= 0) {
 				x1 = Math.Sqrt(y1);
 				x2 = -x1;
@@ -82,8 +90,7 @@ public class main {
 					Console.WriteLine("x3 = {0}, x4 = {1}", x3, x4);
 				else 
 					Console.WriteLine("x1 = {0}, x2 = {1}", x3, x4);
-			}
-						
+			}			
 		}
 	}
 }
