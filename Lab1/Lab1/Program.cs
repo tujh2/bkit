49056b4b72a26a8be2369bdfd1a0306166e4a905
Egor Sysoykin <esisoykin@gmail.com>
2019-09-26 15:04:15 +0300

    fixed input and alg

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,45 +1,41 @@
 using System;
 namespace Lab1 {
 	public class Program {
-	
+		public static void ParseParam(String a, String name, ref double b, bool flag) {
+			while(!Double.TryParse(a, out b)) {
+				Console.ForegroundColor = ConsoleColor.Red;
+				if(flag)
+	                                Console.WriteLine("Error parsing param, put it from keyboard" + a);
+                                Console.ResetColor();
+				Console.Write(name + " = ");
+				a = Console.ReadLine();
+				flag = true;
+			}
+		}	
 		public static void Main(String[] args) {
 			double a = 0, b = 0, c = 0;
 			Console.Title = "Сысойкин Егор ИУ5-34Б";
 			bool flag = true;
-			if(args.Length == 3) {
-				Console.WriteLine("Taking A, B, C from args");
-				flag = false;
-				if(!Double.TryParse(args[0], out a) || !Double.TryParse(args[1], out b) || !Double.TryParse(args[2], out c)) {
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine("Error parsing args");
-					Console.ResetColor();
-					flag = true;
+			if(args.Length > 0) {
+				ParseParam(args[0], "A", ref a, true);
+				if(args.Length > 1) {
+					ParseParam(args[1], "B", ref b, true);
+					if(args.Length > 2)
+						ParseParam(args[2], "C", ref c, true);
+					else
+						ParseParam(null, "C", ref c, false);
+				}
+				else  {
+					ParseParam(null, "B", ref b, false);
+					ParseParam(null, "C", ref c, false);
 				}
 			}
-			if(flag) {
-				Console.WriteLine("Invalid args. Put in A, B, C from keyboard");
-				Console.Write("A = ");
-                		while(!Double.TryParse(Console.ReadLine(), out a)) {
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine("Error parsing A");
-					Console.ResetColor();
-					Console.Write("A= ");
-				}
-                		Console.Write("B = ");
-                		while(!Double.TryParse(Console.ReadLine(), out b)) {
-					Console.ForegroundColor = ConsoleColor.Red;
-                        	        Console.WriteLine("Error parsing B");
-                               		Console.ResetColor();
-                                	Console.Write("B= ");
-				}
-                		Console.Write("C = ");
-	                	while(!Double.TryParse(Console.ReadLine(), out c)) {
-					Console.ForegroundColor = ConsoleColor.Red;
-       		                 	Console.WriteLine("Error parsing C");
-                	                Console.ResetColor();
-					Console.Write("C= ");
-				}
+			else {
+				ParseParam(null, "A", ref a, false);
+				ParseParam(null, "B", ref b, false);
+				ParseParam(null, "C", ref c, false);
 			}
+
 			Console.WriteLine("A = {0} B = {1} C = {2}", a, b, c);
 			double y1, y2;
 			double x1, x2, x3, x4;
@@ -50,23 +46,26 @@ namespace Lab1 {
 				Console.ForegroundColor = ConsoleColor.Red;
 				Console.WriteLine("D < 0; no real roots");		
 			}
-			else if( a == 0 && b == 0 && c != 0) {
-				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine("no roots");
-			}
-			else if(a == 0 && b != 0 && c != 0) {
-				if(-c/b >= 0) {
-					x1 = Math.Sqrt(-c/b);
-					x2 = -x1;
-					Console.WriteLine("x1 = {0}, x2 = {1}", x1, x2);
+			else if(a == 0) {
+				if(b == 0) {
+					if(c == 0) 
+						Console.WriteLine("Anything");
+					else {
+						Console.ForegroundColor = ConsoleColor.Red;
+                                		Console.WriteLine("no roots");
+					}
 				}
 				else {
-					Console.ForegroundColor = ConsoleColor.Red;
-					Console.WriteLine("no real roots");
+					if(-c/b >= 0) {
+                                        	x1 = Math.Sqrt(-c/b);
+                                        	x2 = -x1;
+                                        	Console.WriteLine("x1 = {0}, x2 = {1}", x1, x2);
+                                  	}
+                                  	else {
+                                        	Console.ForegroundColor = ConsoleColor.Red;
+                                        	Console.WriteLine("no real roots");
+                                  	}
 				}
-			}
-			else if(a == 0 && b == 0 && c == 0) {
-				Console.WriteLine("Anything");
 			}
 			else {
 				y1 = (-b + Math.Sqrt(D))/(2*a);
