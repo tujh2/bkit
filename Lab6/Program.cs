48beb86be92d69e755394d32be0f706d2012f0bf
baumanboy <akushko2013@mail.ru>
2019-11-21 10:06:00 +0300

    init Lab6

---
+++ Lab6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab6 {
+    class Program {
+        delegate int SampleDelegate(String a, String b);
+
+        static void Main(string[] args) {
+            SampleDelegate sampleDelegate;
+            sampleDelegate = DelegateLevDistance;
+
+            Console.WriteLine(
+                sampleDelegate("aaaaa", "bbbbb"));
+
+            Console.WriteLine(
+                DelegateAsParam(sampleDelegate, "aaaaa", "bbbbb"));
+
+            Console.WriteLine(
+                DelegateAsParam((String a, String b) => Lab5.LevDistance.Distance(a, b), "aaaaaaaaa", "bbbbb"));
+            Func<String, String, int> func = sampleDelegate;
+            Console.WriteLine(
+                CommonDelegateAsParam(, "aaaaa", "bbbbb"));
+
+            Console.WriteLine(
+                CommonDelegateAsParam((String a, String b) => Lab5.LevDistance.Distance(a, b), "aaaaaaaaaaaaaaa", "bbbbb"));
+        }
+
+        static int DelegateLevDistance(String a, String b) {
+            return Lab5.LevDistance.Distance(a, b);
+        }
+
+        static int DelegateAsParam(SampleDelegate sampleDelegate, String a, String b) {
+            return sampleDelegate(a, b);
+        }
+
+        static int CommonDelegateAsParam(Func<String, String, int> func, String a, String b) {
+            return func(a, b);
+        }
+    } 
+}
